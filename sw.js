54ef6db84fdfedea052e4ec149503d0e70439488
ShiroiWhite <59118798+LoveKirito@users.noreply.github.com>
2025-08-14 14:53:59 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'travel-budget-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating');
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
